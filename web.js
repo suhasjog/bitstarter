11abf83f40953532b20df5eb431a8ad0b5d66156
Suhas Jog <suhas_jog@yahoo.com>
2013-08-07 05:43:33 +0000

    updated web.js and added index.html

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+	var index = fs.readFileSync('index.html')
+  response.send('Hello World2!'+ index);
+
+
 });
 
 var port = process.env.PORT || 5000;
